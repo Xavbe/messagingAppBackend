45462dfe9064ce51de28533221a089cc3a5f3bea
Xavbe <xavier.beaudoin.3@ulaval.ca>
2026-05-23 13:49:32 -0400

    adding security config

---
+++ src/main/java/messagingApp/controller/authentication/SecurityConfig.java
@@ -0,0 +1,33 @@
+package messagingApp.controller.authentication;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Autowired
+    private CookieVerification cookieVerification;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(s -> s
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/login", "/register").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(cookieVerification, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
